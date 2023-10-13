784b48abb355546aaaea336db256a1b386a17a93
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2023-10-13 19:59:21 +0530

    Show toast after click on menu

--- app/src/main/java/com/shruti/practice/PopUpmenu.kt
+++ app/src/main/java/com/shruti/practice/PopUpmenu.kt
@@ -2,6 +2,8 @@ package com.shruti.practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import android.widget.Toast
 import com.shruti.practice.databinding.ActivityPopUpmenuBinding
 
 class PopUpmenu : AppCompatActivity() {
@@ -10,6 +12,17 @@ class PopUpmenu : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPopUpmenuBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-
+        binding?.btnpopup?.setOnClickListener {
+        PopupMenu(this,binding?.btnpopup).apply {
+            menuInflater.inflate(R.menu.popupmenu,menu)
+            setOnMenuItemClickListener {
+                when(it.itemId){
+                    R.id.firstmenu -> Toast.makeText(this@PopUpmenu,"First",Toast.LENGTH_SHORT).show()
+                    R.id.secondmenu -> Toast.makeText(this@PopUpmenu,"Second",Toast.LENGTH_SHORT).show()
+                }
+                return@setOnMenuItemClickListener true
+            }
+        }.show()
+        }
     }
 }
