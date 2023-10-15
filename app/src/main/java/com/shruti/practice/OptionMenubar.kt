40ccec1d771d24ddbf07ea5c7bfc078b435f2214
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2023-10-15 12:48:57 +0530

    Jetpack navigation

--- app/src/main/java/com/shruti/practice/OptionMenubar.kt
+++ app/src/main/java/com/shruti/practice/OptionMenubar.kt
@@ -5,15 +5,30 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.shruti.practice.databinding.ActivityOptionMenubarBinding
 
 class OptionMenubar : AppCompatActivity() {
     lateinit var binding : ActivityOptionMenubarBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOptionMenubarBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        navController = findNavController(R.id.navHost)
+        binding.bottomnav.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.firstmenu ->{
+                    navController.navigate(R.id.firstFragment)
+                }
+                R.id.secondmenu ->{
+                    navController.navigate(R.id.secondFragment)
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
